d414b15e04a25469c7754def7f41cc439e64b334
aagneye-syam <saagneye2003@gmail.com>
2024-08-22 23:47:11 +0530

    OrderDashboard page init

---
+++ lib/OrderDashboard.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class OrderDashboard extends StatelessWidget {
+  const OrderDashboard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}

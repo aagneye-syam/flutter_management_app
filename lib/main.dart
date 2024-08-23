cd942e17c6984bd4b0ef63eb3e79e7b19bd20ba3
aagneye-syam <saagneye2003@gmail.com>
2024-08-23 23:55:12 +0530

    task page inint

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_management_app/OrderDashboard.dart';
+import 'package:flutter_management_app/task.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: OrderDashboard(),
+      home: OrdersPage(),
     );
   }
 }
